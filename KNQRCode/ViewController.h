21afdd9efb43a45a5296af39677a596bbed71249
Leessang <992275294@qq.com>
2017-09-19 10:48:42 +0800

    Initial Commit

---
+++ KNQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNQRCode
+//
+//  Created by 刘凡 on 2017/9/19.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
